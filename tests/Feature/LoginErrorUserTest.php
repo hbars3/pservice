f81f73645f638a80bbd8f1114921f8c47f1bb3e4
Herbert Arias <dayanqwe123@gmail.com>
2021-08-09 14:32:16 -0500

    test: login Error

--- tests/Feature/LoginErrorUserTest.php
+++ tests/Feature/LoginErrorUserTest.php
@@ -9,7 +9,6 @@ use Tests\TestCase;
 
 class LoginErrorUserTest extends TestCase
 {
-    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
--- tests/Feature/LoginUserTest.php
+++ tests/Feature/LoginUserTest.php
@@ -9,7 +9,6 @@ use Tests\TestCase;
 
 class LoginUserTest extends TestCase
 {
-    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
